0eafa721d9be3ad1cdf70a694443fa17e4fee5f7
Yasser Saied <ysaied80@yahoo.com>
2020-02-12 11:16:47 +0400

    Update idle_plcy.py

--- idle_plcy.py
+++ idle_plcy.py
@@ -17,4 +17,4 @@ dev.close()
 plcys = config.findall('policy-options/policy-statement')
 #print (etree.tostring(plcys, encoding='unicode', pretty_print=True))
 for plcy in plcys:
-   print ("Policy name \"%s\" found" % plcy.find('.//name').text)
+   print ("Policy \"%s\" found" % plcy.find('.//name').text)
