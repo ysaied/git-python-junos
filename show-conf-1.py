c41b548d512577449a0c4a0a6d4a6e51959dd4fa
Yasser Saied <ysaied80@yahoo.com>
2020-02-12 23:15:00 +0400

    Update show-conf-1.py

--- show-conf-1.py
+++ show-conf-1.py
@@ -3,25 +3,14 @@
 from jnpr.junos import Device
 from lxml import etree
 
-#kif = ""
-hrz= "10.117.97.55"
-#ams= ""
-lon = "10.117.97.36"
-partner = "10.117.97.39"
-username = "ysaied"
-routers = {hrz, lon, partner}
 
 dev = Device(host="10.117.97.55", user="ysaied")
 
+show_interfaces = etree.XML("<configuration><interfaces/></configuration>")
+
 dev.open()
-#show_rt = dev.rpc.get_route_information()
-show_rt = dev.rpc.get_route_information(table="inet.0")
-#show_rt = dev.rpc.get_route_information(table="inet.0",protocol="direct")
+show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces)
 dev.close()
 
-#print (etree.tostring(show_rt, encoding='unicode', pretty_print=True))
+print (etree.tostring(show_conf_intf, encoding='unicode', pretty_print=True))
 
-dest_rts = show_rt.findall('.//rt')
-
-for dest_rt in dest_rts:
-   print ("Destination \"{}\" via Protocol \"{}\" is active for \"{}\"".format(dest_rt.find('rt-destination').text,dest_rt.find('rt-entry/protocol-name').text,dest_rt.find('rt-entry/age').text))
