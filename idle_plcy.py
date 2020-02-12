b9ecc60a293b467b0d13688f15f86bcb13ca894d
Yasser Saied <ysaied80@yahoo.com>
2020-02-12 14:04:52 +0400

    Update idle_plcy.py

--- idle_plcy.py
+++ idle_plcy.py
@@ -32,7 +32,7 @@ for used_plcy in used_plcys:
 
 unsed_plcys_set = conf_plcys_set - used_plcys_set
 
-print ("There are ". len(unsed_plcys_set) . "unused policies")
+print ("There are ". str(len(unsed_plcys_set)) . " unused policies")
 for unsed_plcy in unsed_plcys_set:
    print ("Policy \"%s\" is unsed" % unsed_plcy)
 
