022bc85d6b7c037126c9ba096350566c79cf3d63
Yasser Saied <ysaied80@yahoo.com>
2020-02-13 12:46:21 +0400

    test

--- show-conf-1.py
+++ show-conf-1.py
@@ -10,14 +10,14 @@ dev = Device(host="10.117.97.39", user="ysaied")
 # below XML filter
 #show_interfaces = etree.XML("<interfaces><interface><name>fxp0</name></interface></interfaces>")
 # below XPATH filter
-show_interfaces = ("interfaces/interface[name = \"fxp0\"]")
+show_interfaces = ('interfaces/interface')
 
 dev.open()
 #below to show output in XML format
-#show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces)
+show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'inherit':'inherit'})
 
 #below to show output in JUNOS Text format
-show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'text', 'inherit':'inherit'})
+#show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'text', 'inherit':'inherit'})
 
 #below to show output in XML format with explicit definition
 #show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'xml'})
@@ -33,3 +33,7 @@ print (etree.tostring(show_conf_intf, encoding='unicode', pretty_print=True))
 #use below to print JSON in human readable format
 #show_conf_intf_json = json.dumps(show_conf_intf, indent=2)
 #print (show_conf_intf_json)
+
+list = show_conf_intf.find('interfaces/interface[name = "fxp0"]/unit[name = "0"]/family/inet/address')
+for item in list:
+   print (item.text)
