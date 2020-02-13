0b08a8b09ebb29437dd4b3598293973821102ad4
Yasser Saied <ysaied80@yahoo.com>
2020-02-13 07:24:41 +0400

    Update show-conf-1.py

--- show-conf-1.py
+++ show-conf-1.py
@@ -2,6 +2,7 @@
 #!/usr/bin/python
 from jnpr.junos import Device
 from lxml import etree
+import json
 
 
 dev = Device(host="10.117.97.39", user="ysaied")
@@ -12,12 +13,15 @@ dev.open()
 #below to show output in XML format
 #show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces)
 #below to show output in JUNOS Text format
-show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'text'})
+#show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'text'})
 #below to show output in XML format with explicit defination
 #show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'xml'})
-#show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'json'})
+
+show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'json'})
+show_conf_intf_json = json.loads(show_conf_intf)
+
 dev.close()
 
-#print (show_conf_intf)
-print (etree.tostring(show_conf_intf, encoding='unicode', pretty_print=True))
+print (show_conf_intf_json)
+#print (etree.tostring(show_conf_intf, encoding='unicode', pretty_print=True))
 
