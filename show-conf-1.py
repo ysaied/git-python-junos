25bfc43c08b758dc0202ee55082c18036885f0fb
Yasser Saied <ysaied80@yahoo.com>
2020-02-13 07:49:07 +0400

    Update show-conf-1.py

--- show-conf-1.py
+++ show-conf-1.py
@@ -12,19 +12,21 @@ show_interfaces = etree.XML("<configuration><interfaces/></configuration>")
 dev.open()
 #below to show output in XML format
 #show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces)
+
 #below to show output in JUNOS Text format
 #show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'text'})
-#below to show output in XML format with explicit defination
+
+#below to show output in XML format with explicit definition
 #show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'xml'})
 
+#below to show output in JSON format - need below JSON format printing 
 show_conf_intf = dev.rpc.get_config(filter_xml=show_interfaces, options={'format':'json'})
 
 dev.close()
 
-#show_conf_intf_json = json.loads(show_conf_intf)
 
-show_conf_intf_json = json.dumps(show_conf_intf, indent=2)
-
-print (show_conf_intf_json)
 #print (etree.tostring(show_conf_intf, encoding='unicode', pretty_print=True))
 
+#use below to print JSON in human readable format
+show_conf_intf_json = json.dumps(show_conf_intf, indent=2, sort_keys=True)
+print (show_conf_intf_json)
