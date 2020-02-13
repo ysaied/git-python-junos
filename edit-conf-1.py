b738f3d69b60d9910959874d2dd9f483bf514585
Yasser Saied <ysaied80@yahoo.com>
2020-02-13 14:12:15 +0400

    test

--- edit-conf-1.py
+++ edit-conf-1.py
@@ -4,19 +4,16 @@ from jnpr.junos import Device
 from lxml import etree
 from jnpr.junos.utils.config import Config
 
-
-
 dev = Device(host="10.117.97.39", user="ysaied")
 conf = Config(dev)
 
 config = """
-
 system {
     name-server {
-        8.8.8.8;
-        8.8.4.4;
+        8.8.5.5;
+        8.8.3.3;
     }
-}   
+}
 """
 
 dev.open()
@@ -24,16 +21,16 @@ conf.lock()
 
 conf.load(config, format="text")
 
-# below is show compare
-conf.pdiff()
-
-# below is commit check
-print (conf.commit_check())
-
-# below is rollback
-conf.rollback()
+if ( conf.commit_check() & (conf.diff() is not None) ):
+   print ("Configuration is valid and applied")
+   conf.pdiff()
+   conf.commit()
+elif ( conf.commit_check() & (conf.diff() is None) ):
+   print ("Configuration is already thier, nothing to be added!")
+else:
+   print ("Configuration is NOT valid and rollback issued")
+   conf.rollback()
 
 conf.unlock()
-dev.close() 
-
+dev.close()
 
