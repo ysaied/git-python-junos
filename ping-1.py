ac6cdf711469d23daeb2bdaf11d27eb10f45ed4e
Yasser Saied <ysaied80@yahoo.com>
2020-02-18 12:26:55 +0400

    Update ping-1.py

--- ping-1.py
+++ ping-1.py
@@ -64,7 +64,7 @@ def check_ldp_binding(node_name, node_ip):
       
 def check_rsvp_lsp(node_name, node_ip): 
    rpc_rsvp_in_lsp = dev.rpc.get_mpls_lsp_information(ingress=True, up=True)
-   lsp_dst_all = rpc_rpc_rsvp_in_lsp.findall('.//destination-address')
+   lsp_dst_all = rpc_rsvp_in_lsp.findall('.//destination-address')
    if ( lsp_dst_all is not None):
       for lsp_dst in lsp_dst_all:
          if ( lsp_dst.text == node_ip):
