e67adc95a5388796880df396596d46f5529ddc39
Yasser Saied <ysaied80@yahoo.com>
2020-02-18 19:32:42 +0400

    Update save-config-local.py

--- save-config-local.py
+++ save-config-local.py
@@ -11,19 +11,11 @@ dev_mgmt = { "KIF_VPN" : "10.117.97.39" }
 
 login_username = "ysaied"
 
-time_now = datetime.now().strftime("%d%b%Y_%I:%M_%p")
-backup_conf_filename = str("./" + "config-backup-" + time_now + ".gz")
-print (backup_conf_filename)
+#time_now = datetime.now().strftime("%d%b%Y_%I:%M_%p")
+backup_conf_filename = str("./" + "config-backup-" + datetime.now().strftime("%d%b%Y_%I:%M_%p") + ".gz")
 
 for src_node, mgmt_ip in dev_mgmt.items():
    dev = Device(host= mgmt_ip, user= login_username)
-
    dev.open()
    file_copy = dev.rpc.file_copy(source="/config/juniper.conf.gz", destination=backup_conf_filename)   
-#   print ("\n" + "="*20 + " "*2 + src_node + " "*2 + "="*20)
-
-
-#   print ("="*(44+len(src_node)) + "\n")
-   
-   
    dev.close()
