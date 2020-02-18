045d90ecc55fc331c11a09d5442f43eab512bdd1
Yasser Saied <ysaied80@yahoo.com>
2020-02-18 11:41:47 +0400

    Update ping-1.py

--- ping-1.py
+++ ping-1.py
@@ -25,6 +25,13 @@ def check_reachabilitily(node_name, node_ip):
    else:
       print ("Destination %s is NOT Reachable!" % node_name )
 
+def check_ospf_route(node_name, node_ip): 
+   rpc_ospf_rt = dev.rpc.get_route_information(protocol="ospf", table="inet.0", destination=node_ip)
+   if ( rpc_ospf_rt.find('.//rt-destination').text == node_ip ):
+      print ("OSPF route to %s is Available!" % node_name )
+   else:
+      print ("OSPF route to %s is NOT Available!" % node_name )
+
 for src_node, mgmt_ip in dev_mgmt.items():
    dev = Device(host= mgmt_ip, user= login_username)
    dev.open()
@@ -33,7 +40,10 @@ for src_node, mgmt_ip in dev_mgmt.items():
    
    for dst_node, loopback_ip in dev_loopback.items():
       if ( dst_node != src_node ):
-         check_reachabilitily(dst_node, loopback_ip)   
+         check_reachabilitily(dst_node, loopback_ip)
+         check_ospf_route(dst_node, loopback_ip)   
+         
+
 
    print ("="*(44+len(src_node)) + "\n")
    
