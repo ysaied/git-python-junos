b673f3517e8bf00f5a72d22254410dd3f4d0beed
Yasser Saied <ysaied80@yahoo.com>
2020-02-18 10:27:02 +0400

    Update ping-1.py

--- ping-1.py
+++ ping-1.py
@@ -5,20 +5,26 @@ from lxml import etree
 import json
 
 
-dev_dic = { "KIF_VPN" : "10.117.97.56", 
+dev_mgmt = { "KIF_VPN" : "10.117.97.56", 
    "HRZ_VPN" : "10.117.97.55", 
    "AMS_VPN" : "10.117.97.37", 
-   "LON_VPN" : "10.117.97.36", 
-   "Partner" : "10.117.97.39" 
+   "LON_VPN" : "10.117.97.36" 
+   } 
+dev_loopback = { "KIF_VPN" : "1.0.0.11", 
+   "HRZ_VPN" : "1.0.0.12", 
+   "AMS_VPN" : "1.0.0.13", 
+   "LON_VPN" : "1.0.0.14"
    }  
 
 login_username = "ysaied"
 
-for node, ip in dev_dic.items():
-   dev = Device(host= ip, user= login_username)
+for src_node, mgmt_ip in dev_mgmt.items():
+   dev = Device(host= mgmt_ip, user= login_username)
    dev.open()
-   ping_mgmt_gw = dev.rpc.ping(host="10.117.97.1", count="10", rapid=True)
-   if ( ping_mgmt_gw.find('.//ping-success') is not None):
-      print ("Destination %s reachable" % "10.117.97.1")
-   print (etree.tostring(ping_mgmt_gw, encoding='unicode', pretty_print=True))
+   for dst_node, loopback_ip in dev_loopback.items():
+      ping_dst_loopback = dev.rpc.ping(host=dst_node, count="10", rapid=True)
+      if ( ping_dst_loopback.find('.//ping-success') is not None):
+         print ("Destination %s is Reachable!" % dst_node )
+      else:
+         print ("Destination %s is NOT Reachable!" % dst_node )
    dev.close()
