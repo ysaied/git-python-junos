a07c8a4ceebe577bd5ddf49410e2e0ac765289e3
Yasser Saied <ysaied80@yahoo.com>
2020-02-19 11:13:21 +0400

    test

--- device-info.py
+++ device-info.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/python
 from jnpr.junos import Device
 from lxml import etree
 import json
 from jnpr.junos.utils.fs import FS
 from datetime import datetime
-
+from pprint import pprint
 
 dev_mgmt = { "KIF_VPN" : "10.117.97.56", 
    "HRZ_VPN" : "10.117.97.55", 
@@ -19,8 +18,11 @@ login_username = "ysaied"
 
 for src_node, mgmt_ip in dev_mgmt.items():
    dev = Device(host= mgmt_ip, user= login_username)
-   dev_info = dev.facts()
-   print (dev_info)
-
-#   dev.open()
-#   dev.close()
+   dev.open()
+   dev_info = dev.facts
+#   pprint (dev_info)
+   print ( "version {} model {}".format(dev.facts["version_RE1"],dev.facts["model"]))
+#   for key, value in dev_info.items():
+#      print ("{} ----> {}".format(key, value))
+   print ("="*50)
+   dev.close()
