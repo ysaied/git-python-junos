37cb016df3b94f079cba709c056e6ec2531889a0
Yasser Saied <ysaied80@yahoo.com>
2020-03-12 21:59:27 +0400

    Update network-ops-1.py

--- network-ops-1.py
+++ network-ops-1.py
@@ -70,7 +70,7 @@ for c in range(2,6):
 time_now = datetime.now().strftime("%A__%d-%h-%Y__%I:%M %p")
 show_all = show_a + show_b + show_c
 
-file_output = open("~/junos-output.txt", "w")
+file_output = open("junos-output.txt", "w")
 
 for src_node, mgmt_ip in dev_mgmt.items():
    dev = Device(host= mgmt_ip, user= login_username)
