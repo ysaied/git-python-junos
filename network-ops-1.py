c2c3d6e08b31608bbde763ed174b98f01e7abfe4
Yasser Saied <ysaied80@yahoo.com>
2020-03-14 14:06:53 +0400

    Update network-ops-1.py

--- network-ops-1.py
+++ network-ops-1.py
@@ -76,6 +76,7 @@ for src_node, mgmt_ip in dev_mgmt.items():
    dev = Device(host= mgmt_ip, user= login_username)
    dev.open()
    
+   print >> file_output, (("#"*(len(src_node) + len(time_now) + 46))"\n"*2)
    print >> file_output, ("*"*(len(src_node) + len(time_now) + 46))
    print >> file_output, ("\n" + "="*20 + " "*2 + src_node + " "*2 + time_now + " "*2 + "="*20)
    
