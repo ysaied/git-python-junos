7be17fa541491f5c77ae8c8cf4400d71e6e476ea
Yasser Saied <ysaied80@yahoo.com>
2020-03-16 23:22:17 +0400

    Update network-ops-1.py

--- network-ops-1.py
+++ network-ops-1.py
@@ -71,11 +71,17 @@ for c in range(2,6):
   show_cmd = vars()[("show_c%d" % c)]
   show_c.append(show_cmd)
 
+show_d = list()
+show_d0 = "show configuration"
 
+for d in range(0):
+  show_cmd = vars()[("show_d%d" % d)]
+  show_d.append(show_cmd)
 
 time_now = datetime.now().strftime("%A__%d-%h-%Y__%I:%M %p")
 today = datetime.now().strftime("%d-%h-%Y")
-show_all = show_a + show_b + show_c
+
+show_all = show_a + show_b + show_c + show_d
 
 #file_output = open("junos-output.txt", "w")
 
