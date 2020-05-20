8cf72e1050a24fab60a2a384f7c9a516640425b1
Yasser Saied <ysaied80@yahoo.com>
2020-05-20 17:33:42 +0400

    all

--- junos-upgrade-testplan-3-2.py
+++ junos-upgrade-testplan-3-2.py
@@ -17,29 +17,36 @@ login_username = "ysaied"
 
 
 
-test_3_1 = list()
-test_3_1_1 = "show chassis hardware"
-test_3_1_2 = "show chassis hardware clei-models"
-test_3_1_3 = "show chassis fpc detail"
-test_3_1_4 = "show chassis fpc errors"
-test_3_1_5 = "show chassis fpc pic-status"
-test_3_1_6 = "show chassis alarms"
-test_3_1_7 = "show system alarms"
-test_3_1_8 = "show version invoke-on all-routing-engines"
-test_3_1_9 = "show chassis routing-engine"
+test_3_2 = list()
+test_3_2_1 = "show interfaces terse"
+test_3_2_2 = "show ospf interface"
+test_3_2_3 = "show ospf neighbor"
+test_3_2_4 = "show ospf database"
+test_3_2_5 = "show ted database"
+test_3_2_6 = "show route protocol ospf"
+test_3_2_7 = "show mpls interface"
+test_3_2_8 = "show ldp interface"
+test_3_2_9 = "show ldp neighbor"
+test_3_2_10 = "show route table inet.3 protocol ldp"
+test_3_2_11 = "show rsvp interface"
+test_3_2_12 = "show rsvp neighbor"
+test_3_2_13 = "show rsvp session"
+test_3_2_14 = "show route table inet.3 protocol rsvp"
+test_3_2_15 = "show mpls lsp ingress"
+test_3_2_16 = "show bgp summary"
 
-for a in range(1,9):
-  show_cmd = vars()[("test_3_1_%d" % a)]
-  test_3_1.append(show_cmd)
+for a in range(1,16):
+  show_cmd = vars()[("test_3_2_%d" % a)]
+  test_3_2.append(show_cmd)
 
 time_now = datetime.now().strftime("%A__%d-%h-%Y__%I:%M %p")
 today = datetime.now().strftime("%d-%h-%Y")
 
-show_all = test_3_1
+show_all = test_3_2
 
 for src_node, mgmt_ip in dev_mgmt.items():
       
-   file_name = src_node+"_"+"JUNOS-18_4R3-Upgrade-TestPlan3_2"+today+"_outputs.txt"
+   file_name = src_node+"_"+"JUNOS-18_4R3-Upgrade-TestPlan-3_2"+today+"_outputs.txt"
    file_output = open(file_name, "w")
    print ("open file for %s" % src_node)
    
