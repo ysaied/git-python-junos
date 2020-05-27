bfae386335f362ec1e9e71377ab4fdce6544a536
Yasser Saied <ysaied80@yahoo.com>
2020-05-28 00:36:42 +0400

    Create junos-upgrade-testplan-3-5_AMS.py

---
+++ junos-upgrade-testplan-3-5_AMS.py
@@ -0,0 +1,56 @@
+
+#!/usr/bin/python
+from jnpr.junos import Device
+from lxml import etree
+import json
+from jnpr.junos.utils.scp import SCP
+from datetime import datetime
+
+dev_mgmt = { "AMS_VPN" : "10.117.97.37" } 
+
+login_username = "ysaied"
+
+
+test_3_5 = list()
+test_3_5_1 = "show configuration firewall policer plcr-5Mbps "
+test_3_5_2 = "show configuration interfaces ge-0/2/9 "
+
+
+for a in range(1,3):
+  show_cmd = vars()[("test_3_5_%d" % a)]
+  test_3_4.append(show_cmd)
+
+time_now = datetime.now().strftime("%A__%d-%h-%Y__%I:%M %p")
+today = datetime.now().strftime("%d-%h-%Y")
+
+show_all = test_3_4
+
+for src_node, mgmt_ip in dev_mgmt.items():
+      
+   file_name = src_node+"-"+"JUNOS-18_4R3-Upgrade_TestPlan_3_5-Date:"+today+"_outputs.txt"
+   file_output = open(file_name, "w")
+   print ("open file for %s" % src_node)
+   
+   dev = Device(host= mgmt_ip, user= login_username)
+   dev.open()
+
+   print >> file_output, ("#"*(len(src_node) + len(time_now) + 46))
+   print >> file_output, ("\n" + "="*20 + " "*2 + src_node + " "*2 + time_now + " "*2 + "="*20)
+   
+   for show in show_all:
+      print >> file_output, ("\n" + "="*5 + " "*2 + show + " @ " + src_node + " "*2 + "="*5)          
+      try :
+         output = dev.rpc.cli(show, format='text')
+         if type(output) is bool:
+            print >> file_output, ("NO Output Available !!!!")
+         else:
+            print >> file_output, (output.text)
+            print ("\t progressing %s" % show)
+      except:
+         print >> file_output, ("NOT Supported command !!!!")
+   
+   print >> file_output, ("\n" + "="*(44+len(src_node)+len(time_now)) + "\n")   
+   dev.close()
+   file_output.close()
+   print ("closing file for %s" % src_node)
+
